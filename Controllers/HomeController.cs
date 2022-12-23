488ad476002d93fb37d87fd5070f86fdd3e4ce12
Türkalp Kucur <turkalp.kucur@gmail.com>
2022-12-23 09:18:43 +0300

    firstcommit
    firstcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
+using testscroll.Model;
+
+namespace testscroll.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult _PopulatePartial()
+        {
+            List<Model1> L = new List<Model1>();
+            for (int i = 1; i < 200; i++)
+            {
+                Model1 M = new Model1()
+                {
+                    Id=i,
+                    Text="TESTTEXT"+i
+                };
+                L.Add(M);
+            }
+            return PartialView(L);
+        }
+    }
+}
---
+++ Model/Model1.cs
@@ -0,0 +1,8 @@
+﻿namespace testscroll.Model
+{
+    public class Model1
+    {
+        public int Id  { get; set; }
+        public string Text { get; set; }
+    }
+}
